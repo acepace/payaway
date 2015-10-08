d062ee596a90f7db005a772489921325cb88f64d
galpe <galpeee@gmail.com>
2015-10-08 22:03:33 +0300

    add a action bar of check out

--- app/src/main/java/dgd/payaway/TestBarcode.java
+++ app/src/main/java/dgd/payaway/TestBarcode.java
@@ -1,12 +1,17 @@
 package dgd.payaway;
 
+import android.graphics.Point;
+import android.hardware.Camera;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
+import android.view.Display;
+import android.view.Surface;
 import android.view.View;
+import android.widget.LinearLayout;
 
 
 import me.dm7.barcodescanner.zbar.Result;
@@ -23,8 +28,19 @@ public class TestBarcode extends AppCompatActivity implements ZBarScannerView.Re
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_test_barcode);
-        mScannerView = new ZBarScannerView(this);    // Programmatically initialize the scanner view
-        setContentView(mScannerView);                // Set the scanner view as the content view
+        mScannerView = new ZBarScannerView(this);
+        Display display = getWindowManager().getDefaultDisplay();
+        Point size = new Point();
+        display.getSize(size);
+        int width = size.x;
+//        LinearLayout.LayoutParams layoutParams = new LinearLayout.LayoutParams(width, 500);
+//
+//        android.hardware.Camera.CameraInfo info =
+//                new android.hardware.Camera.CameraInfo();
+//        android.hardware.Camera.getCameraInfo(Camera.CameraInfo.CAMERA_FACING_BACK, info);
+                      // Set the scanner view as the content view
+        LinearLayout linearLayout = (LinearLayout) findViewById(R.id.zk_layout);
+        linearLayout.addView(mScannerView);
     }
 
     @Override
